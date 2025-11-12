6f01f7da448a443ff10ac7ec763c71e05605679c
Pruthvi1718 <mrpruthvi24@gmail.com>
2025-11-13 00:18:04 +0530

    sql commands

--- first.sql
+++ first.sql
@@ -1,26 +1,57 @@
-INSERT INTO Customers (customer_id, first_name, last_name, age, country)
-VALUES (6, 'Joe', 'Roy', 30, 'USA');
+--First Sql Code
+mysql --version
 
-SELECT * FROM Customers;
+mysql -u root -p
 
-UPDATE Customers
-SET age = 32
-WHERE customer_id = 1;
+show databases;
 
-DELETE FROM Customers
-WHERE customer_id = 5;
+create database Demo;
 
-//filter
-SELECT * FROM Customers
-WHERE country = 'USA';
+show databases;
 
-SELECT * FROM Customers
-WHERE age BETWEEN 20 AND 30;
+use demo;
 
-SELECT * FROM Customers
-WHERE first_name LIKE 'J%';
+create table Student(
+    S_id int,
+    S_Name VARCHAR(100),
+    S_Dept VARCHAR(100),
+    S_Section VARCHAR(100)
+);
 
-SELECT * FROM Customers
-WHERE country IN ('UK', 'UAE');
+select * from Student;
 
-//aggregators
+insert into Student(S_id, S_Name, S_Dept, S_section) 
+values(01, 'ARU', 'CS', 'A'),
+(02,'BABU','DSA','B'),
+(03,'CHARU','EEE','C'),
+(04,'DAS','AI','D'),
+(05,'FAS','ML','E');
+
+select * from Student;
+
+--Filters
+--WHERE
+SELECT * FROM Student WHERE S_Dept = 'CS';
+
+--LIKE
+SELECT * FROM Student WHERE S_Name LIKE 'C%';
+
+--BETWEEN
+SELECT * FROM Student WHERE S_id BETWEEN 2 AND 4;
+
+--IN
+SELECT * FROM Student WHERE S_Dept IN ('AI', 'ML');
+
+--Aggregators
+--Count
+SELECT COUNT(*) AS Total_Students FROM Student;
+
+--Max
+SELECT MAX(S_id) AS Max_ID FROM Student;
+
+--Min
+SELECT MIN(S_id) AS Min_ID FROM Student;
+
+--Group by
+SELECT S_Dept, COUNT(*) AS Dept_Count FROM Student GROUP BY S_Dept;
+
