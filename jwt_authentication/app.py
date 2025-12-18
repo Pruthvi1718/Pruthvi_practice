c1032cc84f7fee0f0d8ca1dea27cf5e43acc80b8
Pruthvi1718 <mrpruthvi24@gmail.com>
2025-12-18 05:33:02 +0530

    jwt auth

--- jwt_authentication/app.py
+++ jwt_authentication/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "super-secret-key"
 jwt = JWTManager(app)
 
-# Dummy user storage
+# Dummy storage
 users = {}
 
+#ROUTE CREATION
 @app.route("/")
 def home():
     return redirect(url_for("login"))
@@ -27,7 +28,7 @@ def signup():
     return render_template("signup.html")
 
 
-# ---------------- LOGIN ----------------
+#LOGIN
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -49,5 +50,6 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(message=f"Hello {current_user}, this is a protected route")
 
+#RUN & DEBUG
 if __name__ == "__main__":
     app.run(debug=True)
