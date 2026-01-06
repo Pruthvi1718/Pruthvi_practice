adca97fc5292f1c07192059b6a130b44253943d9
Pruthvi1718 <mrpruthvi24@gmail.com>
2026-01-07 00:25:07 +0530

    django_blog

--- django_tutorial/MYBLOG/myblog/blog/views.py
+++ django_tutorial/MYBLOG/myblog/blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to My Blogs")
+    return HttpResponse("<h1>Welcome to My Blogs</h1>")
